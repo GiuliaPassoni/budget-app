805824c39adb48a5e9710af5b171a50afb3a4856
Giulia Passoni <kirakira95@msn.com>
2024-05-23 19:42:07 +0200

    feat: update piechart with props.

--- src/components/atoms/PieChart.tsx
+++ src/components/atoms/PieChart.tsx
@@ -1,18 +1,18 @@
 import { select } from "d3";
 import { onMount } from "solid-js";
 
-export default function Circle() {
+export default function PieChart(props: { w: number; h: number; r: number }) {
   onMount(() => {
     const svg = select("div#my_dataviz")
       .append("svg")
       .attr("viewBox", "0 0 100 100")
-      .attr("width", 500)
-      .attr("height", 500)
+      .attr("width", props.w)
+      .attr("height", props.h)
       .append("circle")
       .attr("fill", "black")
       .attr("cx", "50")
       .attr("cy", "50")
-      .attr("r", "25");
+      .attr("r", props.r);
   });
   return <div id="my_dataviz"></div>;
 }
