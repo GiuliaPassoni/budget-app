527deb574a0287951c89c9c19d8c90aaf80735d7
Giulia Passoni <kirakira95@msn.com>
2024-05-27 19:44:53 +0200

    style: update overview.

--- src/components/molecules/ChartCard.tsx
+++ src/components/molecules/ChartCard.tsx
@@ -8,23 +8,32 @@ import InputModal from "~/components/molecules/InputModal";
 
 export default function ChartCard() {
   const [showModal, setshowModal] = createSignal(false);
-  function handleClick (){
+  function handleClick() {
     setshowModal(!showModal());
-  };
+  }
 
   createEffect(() => {
-    console.debug(showModal())
-  })
+    console.debug(showModal());
+  });
 
   return (
-    <BaseCard class="mx-auto center-text" bg="success" style="max-width: 25vw">
+    <BaseCard
+      class="mx-auto center-text"
+      style={{ padding: "2rem", width: "25vw" }}
+      bg="light"
+      border="info"
+    >
       <Card.Body>
         <Card.Title>Expenses overview</Card.Title>
         {/*date picker*/}
         {/*<Card.Text>*/}
         <PieChart w={50} h={50} r={50} />
         {/*</Card.Text>*/}
-        <BaseButton onClick={handleClick} variant="warning" style="border-radius:50%">
+        <BaseButton
+          onClick={handleClick}
+          variant="warning"
+          style="border-radius:50%"
+        >
           +
         </BaseButton>
       </Card.Body>
@@ -32,7 +41,7 @@ export default function ChartCard() {
         showModal={showModal()}
         handleClose={() => {
           setshowModal(false);
-          console.debug("hi")
+          console.debug("hi");
         }}
       />
     </BaseCard>
