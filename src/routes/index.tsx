6d5a79be69279ee31f09032080450e0eda8573b7
Giulia Passoni <kirakira95@msn.com>
2024-05-27 19:00:03 +0200

    feat: add basic error boundary for app.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,24 +2,29 @@ import { Title } from "@solidjs/meta";
 import Counter from "~/components/Counter";
 import BaseButton from "~/components/baseComponents/BaseButton";
 import BaseCard from "~/components/baseComponents/BaseCard";
-import Circle from "~/components/atoms/PieChart";
+import PieChart from "~/components/atoms/PieChart";
+import { ErrorBoundary } from "solid-js";
 
 export default function Home() {
   return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <BaseButton variant="outline-secondary" size="sm">
-        Hi
-      </BaseButton>
-      <BaseCard>
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </BaseCard>
-      <Circle />
-    </main>
+    <ErrorBoundary
+      fallback={(err, reset) => <div onClick={reset}>Error: {err.toString()}</div>}
+    >
+      <main>
+        <Title>Hello World</Title>
+        <h1>Hello world!</h1>
+        <Counter />
+        <BaseButton variant="outline-secondary" size="sm">
+          Hi
+        </BaseButton>
+        <BaseCard>
+          <a href="https://start.solidjs.com" target="_blank">
+            start.solidjs.com
+          </a>{" "}
+          to learn how to build SolidStart apps.
+        </BaseCard>
+        <PieChart w={10} h={10} r={100} />
+      </main>
+    </ErrorBoundary>
   );
 }
