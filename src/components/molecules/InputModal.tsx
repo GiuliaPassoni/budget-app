82fabdf951a0cc485bcadf68327e9557ce4beed6
Giulia Passoni <kirakira95@msn.com>
2024-05-27 18:59:40 +0200

    feat: input modal and tabs (BASIC).

--- src/components/molecules/InputModal.tsx
+++ src/components/molecules/InputModal.tsx
@@ -1,16 +1,31 @@
 import BaseButton from "~/components/baseComponents/BaseButton";
 import BaseModal from "~/components/baseComponents/BaseModal";
-import { ModalProps } from "solid-bootstrap";
-import { createEffect } from "solid-js";
+import {
+  ModalProps,
+  Tab,
+  Tabs,
+} from "solid-bootstrap";
+import { createSignal } from "solid-js";
+import InputModalTabContent from "~/components/molecules/InputModalTabContent";
 
 export default function InputModal(props: Partial<ModalProps>){
-  console.debug(props)
+  const [key, setKey] = createSignal('home');
+
   return (
     <BaseModal
       showModal={props.showModal}
       handleClose={props.handleClose()}
-      title="hi"
-      body={<>This is a modal</>}
+      title="Record your transaction"
+      body={
+          <Tabs>
+            <Tab eventKey="expense" title="Expense">
+              <InputModalTabContent/>
+            </Tab>
+            <Tab eventKey="income" title="Income">
+              <InputModalTabContent/>
+            </Tab>
+          </Tabs>
+      }
       footer={
         <>
           <BaseButton onClick={props.handleClose()}>Hi</BaseButton>
