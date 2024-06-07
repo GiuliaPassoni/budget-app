4bf60d9bd213a343e5015c40485890ba56dfc3f8
Giulia Passoni <kirakira95@msn.com>
2024-06-07 09:44:17 +0200

    feat: removed unused export for app.tsx. chore: prettify everything.

--- src/app.tsx
+++ src/app.tsx
@@ -5,17 +5,17 @@ import { Suspense } from "solid-js";
 import "./app.css";
 import NavBar from "~/components/organisms/NavBar";
 
-export default function App() {
-  return (
-    <Router
-      root={(props) => (
-        <MetaProvider>
-          <NavBar />
-          <Suspense>{props.children}</Suspense>
-        </MetaProvider>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+function App() {
+	return (
+		<Router
+			root={(props) => (
+				<MetaProvider>
+					<NavBar />
+					<Suspense>{props.children}</Suspense>
+				</MetaProvider>
+			)}
+		>
+			<FileRoutes />
+		</Router>
+	);
 }
