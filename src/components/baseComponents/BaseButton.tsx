1051d2c1c48e2e5247ff7e8771739207784b1492
Giulia Passoni <kirakira95@msn.com>
2024-06-16 19:13:35 +0200

    feat: add TODO.

--- src/components/baseComponents/BaseButton.tsx
+++ src/components/baseComponents/BaseButton.tsx
@@ -1,5 +1,6 @@
 import { Button } from "solid-bootstrap";
 
+// TODO check out https://tailwindcss.com/docs/reusing-styles for reusability
 export default function BaseButton(props: any) {
 	const { ...rest } = props;
 	return <Button {...rest}></Button>;
