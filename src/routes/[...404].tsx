eb41f404d6c6d4009eb7b81a119070a8f372a9f2
Giulia Passoni <kirakira95@msn.com>
2024-07-01 18:21:30 +0200

    fix: hydration error.

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,12 +1,7 @@
-import { Title } from "@solidjs/meta";
-import { HttpStatusCode } from "@solidjs/start";
-
 export default function NotFound() {
 	return (
-		<main>
-			<Title>Not Found</Title>
-			<HttpStatusCode code={404} />
+		<>
 			<h1>Ops! Page Not Found</h1>
-		</main>
+		</>
 	);
 }
