63f699e6b1118133996dcdf5873b7e3dc7622082
Giulia Passoni <kirakira95@msn.com>
2024-07-02 08:56:57 +0200

    feat: update basecard with tailwind.

--- src/components/baseComponents/BaseCard.tsx
+++ src/components/baseComponents/BaseCard.tsx
@@ -1,6 +1,14 @@
-import { Card } from "solid-bootstrap";
-
 export default function BaseCard(props: any) {
-	const { ...rest } = props;
-	return <Card {...rest} />;
+	return (
+		<>
+			<div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+					{props.title}
+				</h5>
+				<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+					{props.children}
+				</p>
+			</div>
+		</>
+	);
 }
