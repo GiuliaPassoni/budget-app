2f739d0299bc0c905740e454571617e76e0d5b34
Giulia Passoni <kirakira95@msn.com>
2024-07-11 18:50:53 +0200

    style: PlusIconButton.tsx.

--- src/components/atoms/CardWithIcon.tsx
+++ src/components/atoms/CardWithIcon.tsx
@@ -1,16 +1,18 @@
-export default function CardWithIcon(props: any) {
+import { Show } from "solid-js";
+
+export default function CardWithIcon({ icon, title, children }: any) {
 	return (
 		<a
 			href="#"
-			class="max-w-md p-4 px-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+			class="block max-w-100px p-2 m-2 px-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
 		>
-			<span class="max-w-12 mx-auto">{props.icon}</span>
-			<h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-				{props.title}
-			</h5>
-			{/*<p class="mb-3 font-normal text-gray-500 dark:text-gray-400">*/}
-			{/*{props.children}*/}
-			{/*</p>*/}
+			<div class="block mx-auto w-fit">{icon}</div>
+			<h4 class="my-1 mx-auto w-fit text-gray-900 dark:text-white">{title}</h4>
+			<Show when={children}>
+				<p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+					{children}
+				</p>
+			</Show>
 		</a>
 	);
 }
