7267719cc5bfb61d8483abf245abf0b0ecc5251f
Giulia Passoni <kirakira95@msn.com>
2024-09-10 19:13:44 +0200

    refactor: tidy up.

--- src/components/molecules/NewTransactionModal.tsx
+++ src/components/molecules/NewTransactionModal.tsx
@@ -6,7 +6,7 @@ import PlusIconButton from '~/components/baseComponents/PlusIconButton';
 import CardWithIcon from '~/components/atoms/CardWithIcon';
 import StarIcon from '~/components/atoms/icons/StarIcon';
 import { toast, Toaster } from 'solid-toast';
-import { addExpense } from '~/helpers/expenses_api_helpers';
+import { addExpense, addIncome } from '~/helpers/expenses_api_helpers';
 import allCurrencies from '~/helpers/mock_values_helpers';
 
 interface ModalProps {
@@ -35,21 +35,23 @@ export default function NewTransactionModal(props: ModalProps) {
   }
 
   async function handleSubmit() {
+    // TODO refactor the below to be all props of one single signal-object
     if (amount() && currency() && exchange() && category()) {
+      const transaction = {
+        amount: amount(),
+        currency: currency(),
+        exchange_to_default: exchange(),
+        notes: note(),
+        date: new Date(),
+        ctg_name: category(),
+      };
       if (!method) {
         toast.error('Please specify transaction type');
       }
       if (method() === 'expenses') {
-        await addExpense({
-          amount: amount(),
-          currency: currency(),
-          exchange_to_default: exchange(),
-          notes: note(),
-          date: new Date(),
-          ctg_name: category(),
-        });
+        await addExpense(transaction);
       } else if (method() === 'income') {
-        console.debug('income!');
+        await addIncome(transaction);
       }
     } else {
       toast.error('Missing input');
@@ -61,45 +63,6 @@ export default function NewTransactionModal(props: ModalProps) {
   return (
     <Show when={props.showModal}>
       <div id="default-styled-tab-content">
-        {/*<div class="mb-4 border-b border-gray-200 dark:border-gray-700">*/}
-        {/*  <ul*/}
-        {/*    class="flex flex-wrap -mb-px text-sm font-medium text-center"*/}
-        {/*    id="default-styled-tab"*/}
-        {/*    data-tabs-toggle="#default-styled-tab-content"*/}
-        {/*    data-tabs-active-classes="text-purple-600 hover:text-purple-600 dark:text-purple-500 dark:hover:text-purple-500 border-purple-600 dark:border-purple-500"*/}
-        {/*    data-tabs-inactive-classes="dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300"*/}
-        {/*    role="tablist"*/}
-        {/*  >*/}
-        {/*    <li class="me-2" role="presentation">*/}
-        {/*      <button*/}
-        {/*        class="inline-block p-4 border-b-2 rounded-t-lg"*/}
-        {/*        id="expense-styled-tab"*/}
-        {/*        data-tabs-target="#styled-expense"*/}
-        {/*        type="button"*/}
-        {/*        role="tab"*/}
-        {/*        aria-controls="expense"*/}
-        {/*        aria-selected="false"*/}
-        {/*        onClick={() => handleTabClick('expenses')}*/}
-        {/*      >*/}
-        {/*        expense*/}
-        {/*      </button>*/}
-        {/*    </li>*/}
-        {/*    <li class="me-2" role="presentation">*/}
-        {/*      <button*/}
-        {/*        class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"*/}
-        {/*        id="income-styled-tab"*/}
-        {/*        data-tabs-target="#styled-income"*/}
-        {/*        type="button"*/}
-        {/*        role="tab"*/}
-        {/*        aria-controls="income"*/}
-        {/*        aria-selected="false"*/}
-        {/*        onClick={() => handleTabClick('income')}*/}
-        {/*      >*/}
-        {/*        income*/}
-        {/*      </button>*/}
-        {/*    </li>*/}
-        {/*  </ul>*/}
-        {/*</div>*/}
         <div
           aria-hidden={props.showModal}
           class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
@@ -169,7 +132,10 @@ export default function NewTransactionModal(props: ModalProps) {
                         class="bg-transparent border-none text-right text-gray-500 text-sm focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                         placeholder="0,00"
                         required={true}
-                        onBlur={(e) => setAmount(Number(e.target.value))}
+                        onBlur={(e) => {
+                          e.preventDefault();
+                          setAmount(Number(e.target.value));
+                        }}
                       />
                     </div>
                     <div class="col-span-4 sm:col-span-1 mx-3 p-2">
