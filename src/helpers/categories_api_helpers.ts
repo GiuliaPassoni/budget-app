cf3deeafe1ccf843ea282d739b75f917399dbbba
Giulia Passoni <kirakira95@msn.com>
2024-09-13 07:58:42 +0200

    feat: add addCateg api method.

---
+++ src/helpers/categories_api_helpers.ts
@@ -0,0 +1,40 @@
+import { addDoc, collection, getDocs } from 'firebase/firestore';
+import { currentUser, db } from '~/firebase';
+import { toast } from 'solid-toast';
+import { CategoryI } from '~/helpers/types';
+
+interface PropsI {
+  category: CategoryI;
+}
+
+export async function addNewCategory(props: PropsI) {
+  try {
+    await addDoc(collection(db, 'users', currentUser(), 'categories'), {
+      ...props.category,
+      user_id: currentUser(),
+    });
+    toast.success('New category added');
+  } catch (e) {
+    console.error('Error adding category: ', e);
+    toast.error('Error adding category');
+  }
+}
+
+export async function getCategories() {
+  try {
+    let allCategories: CategoryI[] = [];
+    const querySnapshot = await getDocs(
+      collection(db, 'users', currentUser(), 'categories'),
+    );
+    querySnapshot.forEach((doc) => {
+      const data = doc.data() as CategoryI; // Type assertion
+      allCategories.push(data);
+    });
+    return allCategories;
+  } catch (e) {
+    console.error(
+      'Error retrieving categories: ',
+      e instanceof Error ? e.message : e,
+    );
+  }
+}
