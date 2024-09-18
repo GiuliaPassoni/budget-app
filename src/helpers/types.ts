0c8228db41fc8b828b245b9b4ff9c8da1030b418
Giulia Passoni <kirakira95@msn.com>
2024-09-18 10:04:08 +0200

    feat: add icon map.

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -1,8 +1,10 @@
+import { iconMap } from '~/components/atoms/icons/helpers';
+
 export type TransactionType = 'expenses' | 'income' | 'investments';
 
 export interface CategoryI {
   name: string;
   colour: string;
-  iconName?: string;
+  iconName?: keyof typeof iconMap;
   type: TransactionType;
 }
