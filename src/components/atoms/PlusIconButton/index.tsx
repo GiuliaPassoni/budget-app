c93ee11c368412881c84dd06f30d0b93fee8fe41
Giulia Passoni <kirakira95@msn.com>
2024-09-18 10:08:10 +0200

    refactor: + button.

--- src/components/atoms/PlusIconButton/index.tsx
+++ src/components/atoms/PlusIconButton/index.tsx
@@ -1,13 +1,38 @@
 // TODO check out https://tailwindcss.com/docs/reusing-styles for reusability
 // TODO add tooltip
-export default function PlusIconButton({ handleClick, title, type }: any) {
+
+type Variant = 'primary' | 'secondary';
+interface PropsI {
+  handleClick: (e: Event) => void | Promise<void>;
+  title: string;
+  type: any;
+  variant: Variant; // Example;
+}
+export default function PlusIconButton({
+  title,
+  type,
+  handleClick,
+  variant = 'primary',
+}: PropsI) {
+  const variants: { [key in 'primary' | 'secondary']: string } = {
+    primary: 'bg-blue-400',
+    secondary: 'bg-teal-400',
+  };
+
   return (
     <>
       <button
-        data-tooltip-target="tooltip-light"
+        data-tooltip-target="plus-tooltip"
+        data-tooltip-style="light"
         type={type}
         onClick={handleClick}
-        class="mb-8 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class={` 
+        mb-8 p-2.5 me-2 rounded-full 
+        text-white ${variants[variant]} text-sm text-center font-medium   
+        inline-flex items-center 
+        hover:bg-blue-800 
+        focus:ring-4 focus:outline-none focus:ring-blue-300 
+        dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
       >
         <svg
           class="w-6 h-6 text-white dark:text-white"
@@ -27,16 +52,18 @@ export default function PlusIconButton({ handleClick, title, type }: any) {
           />
         </svg>
         <span class="sr-only">{title}</span>
+        <span>{title}</span>
       </button>
-      <div
-        id="tooltip-light"
-        role="tooltip"
-        data-tooltip-trigger="hover"
-        class="absolute z-100 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 tooltip"
-      >
-        {title}
-        <div class="tooltip-arrow" data-popper-arrow="Test tooltip"></div>
-      </div>
+      {/*TODO fix tooltip*/}
+      {/*<div*/}
+      {/*  id="plus-tooltip"*/}
+      {/*  role="tooltip"*/}
+      {/*  data-tooltip-trigger="hover"*/}
+      {/*  class="absolute z-100 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 tooltip"*/}
+      {/*>*/}
+      {/*  {title}*/}
+      {/*  <div class="tooltip-arrow" data-popper-arrow="Test tooltip"></div>*/}
+      {/*</div>*/}
     </>
   );
 }
--- src/components/organisms/CategoriesComponent.tsx
+++ src/components/organisms/CategoriesComponent.tsx
@@ -3,8 +3,13 @@ import MoonIcon from '~/components/atoms/icons/MoonIcon';
 import SunIcon from '~/components/atoms/icons/SunIcon';
 import Card from '~/components/atoms/Card';
 import CardWithIcon from '~/components/molecules/CardWithIcon';
+import PlusIconButton from '~/components/atoms/PlusIconButton';
+import { createSignal } from 'solid-js';
+import AddCategoryModal from '~/components/molecules/AddCategoryModal';
+import { Toaster } from 'solid-toast';
 
 export default function CategoriesComponent() {
+  const [showModal, setShowModal] = createSignal(false);
   return (
     <>
       <div class="grid grid-cols-3 md:grid-cols-3 gap-4">
@@ -14,6 +19,25 @@ export default function CategoriesComponent() {
         <CardWithIcon icon={<StarIcon />} title="Star income" />
         <CardWithIcon icon={<MoonIcon />} title="Moon income" />
         <CardWithIcon icon={<SunIcon />} title="Sun income" />
+        <CardWithIcon
+          title="Add category"
+          icon={
+            <PlusIconButton
+              variant="secondary"
+              type="submit"
+              handleClick={() => {
+                setShowModal(true);
+              }}
+              title="Add category"
+            />
+          }
+        />
+        <AddCategoryModal
+          showModal={showModal()}
+          handleClose={() => setShowModal(false)}
+          onSubmit={() => setShowModal(false)}
+        />
+        <Toaster />
       </div>
     </>
   );
