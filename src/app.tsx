eb9b04215863251eb3608d90688806b8a8a7d113
giuliapassoni <kirakira95@msn.com>
2024-11-08 08:50:01 +0100

    * feat: implement tanstack in app.tsx.

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,29 @@
-import { MetaProvider, Title } from "@solidjs/meta";
+import { MetaProvider } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
 import NavBar from "~/components/organisms/NavBar";
 import "flowbite";
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
+import { SolidQueryDevtools } from "@tanstack/solid-query-devtools";
+
+const queryClient = new QueryClient();
 
 export default function App() {
 	return (
-		<Router
-			root={(props) => (
-				<MetaProvider>
-					<NavBar />
-					<Suspense>{props.children}</Suspense>
-				</MetaProvider>
-			)}
-		>
-			<FileRoutes />
-		</Router>
+		<QueryClientProvider client={queryClient}>
+			<Router
+				root={(props) => (
+					<MetaProvider>
+						<NavBar />
+						<Suspense>{props.children}</Suspense>
+					</MetaProvider>
+				)}
+			>
+				<FileRoutes />
+			</Router>
+			<SolidQueryDevtools initialIsOpen={true} buttonPosition="bottom-right" />
+		</QueryClientProvider>
 	);
 }
