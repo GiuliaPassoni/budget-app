eca174be8537ebb543246e7fe82193e74d741923
giuliapassoni <kirakira95@msn.com>
2024-11-17 15:30:48 +0100

    * chore: prettify.

--- src/components/molecules/Table/index.tsx
+++ src/components/molecules/Table/index.tsx
@@ -1,76 +1,76 @@
-import { TransactionI } from '~/helpers/expenses_api_helpers';
-import { For } from 'solid-js';
-import { format } from 'date-fns';
+import { TransactionI } from "~/helpers/expenses_api_helpers";
+import { For } from "solid-js";
+import { format } from "date-fns";
 
 interface TableProps {
-  array: TransactionI[] | undefined; // Define the expected prop type
+	array: TransactionI[] | undefined; // Define the expected prop type
 }
 
 // todo these date functions are throwing random dates
 export function formatDate(date: Date | string) {
-  return format(typeof date === 'string' ? new Date(date) : date, 'dd-MM-yyyy');
+	return format(typeof date === "string" ? new Date(date) : date, "dd-MM-yyyy");
 }
 
 function firestoreTimestampToDate(timestamp: {
-  seconds: number;
-  nanoseconds: number;
+	seconds: number;
+	nanoseconds: number;
 }): Date {
-  return new Date(timestamp.seconds * 1000); // Convert seconds to milliseconds
+	return new Date(timestamp.seconds * 1000); // Convert seconds to milliseconds
 }
 
 export default function Table(props: TableProps) {
-  return (
-    <>
-      <div class="relative overflow-x-auto">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <Header />
-          <tbody>
-            {props.array?.length === 0 || !props.array ? (
-              <tr>
-                <td>No data available</td>
-              </tr>
-            ) : (
-              <For each={props.array}>
-                {(transaction) => (
-                  <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <td class="px-6 py-4">{transaction.amount}</td>
-                    <td class="px-6 py-4">{transaction.currency}</td>
-                    <td class="px-6 py-4">
-                      {formatDate(firestoreTimestampToDate(transaction.date))}
-                    </td>
-                    <td class="px-6 py-4">{transaction.ctg_name}</td>
-                    <td class="px-6 py-4">{transaction.notes}</td>
-                  </tr>
-                )}
-              </For>
-            )}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div class="relative overflow-x-auto">
+				<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+					<Header />
+					<tbody>
+						{props.array?.length === 0 || !props.array ? (
+							<tr>
+								<td>No data available</td>
+							</tr>
+						) : (
+							<For each={props.array}>
+								{(transaction) => (
+									<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+										<td class="px-6 py-4">{transaction.amount}</td>
+										<td class="px-6 py-4">{transaction.currency}</td>
+										<td class="px-6 py-4">
+											{formatDate(firestoreTimestampToDate(transaction.date))}
+										</td>
+										<td class="px-6 py-4">{transaction.ctg_name}</td>
+										<td class="px-6 py-4">{transaction.notes}</td>
+									</tr>
+								)}
+							</For>
+						)}
+					</tbody>
+				</table>
+			</div>
+		</>
+	);
 }
 
 function Header() {
-  return (
-    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-      <tr>
-        <th scope="col" class="px-6 py-3">
-          Transaction amount
-        </th>
-        <th scope="col" class="px-6 py-3">
-          Currency
-        </th>
-        <th scope="col" class="px-6 py-3">
-          Date
-        </th>
-        <th scope="col" class="px-6 py-3">
-          Category
-        </th>
-        <th scope="col" class="px-6 py-3">
-          Notes
-        </th>
-      </tr>
-    </thead>
-  );
+	return (
+		<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+			<tr>
+				<th scope="col" class="px-6 py-3">
+					Transaction amount
+				</th>
+				<th scope="col" class="px-6 py-3">
+					Currency
+				</th>
+				<th scope="col" class="px-6 py-3">
+					Date
+				</th>
+				<th scope="col" class="px-6 py-3">
+					Category
+				</th>
+				<th scope="col" class="px-6 py-3">
+					Notes
+				</th>
+			</tr>
+		</thead>
+	);
 }
--- src/helpers/expenses_api_helpers.ts
+++ src/helpers/expenses_api_helpers.ts
@@ -1,28 +1,28 @@
 import {
-  doc,
-  updateDoc,
-  addDoc,
-  collection,
-  getDocs,
-} from 'firebase/firestore';
-import { toast } from 'solid-toast';
-import { currentUser, db } from '~/firebase';
-import { TransactionType } from '~/helpers/types';
+	doc,
+	updateDoc,
+	addDoc,
+	collection,
+	getDocs,
+} from "firebase/firestore";
+import { toast } from "solid-toast";
+import { currentUser, db } from "~/firebase";
+import { TransactionType } from "~/helpers/types";
 
 export interface TransactionI {
-  id?: string;
-  amount: number;
-  currency: string;
-  exchange_to_default: number;
-  notes: string;
-  date: any;
-  ctg_name: string;
+	id?: string;
+	amount: number;
+	currency: string;
+	exchange_to_default: number;
+	notes: string;
+	date: any;
+	ctg_name: string;
 }
 
 // TODO add date to type and methods
-interface PropsI {
-  transactionType: TransactionType;
-  transaction: TransactionI;
+export interface TransactionPropsI {
+	transactionType: TransactionType;
+	transaction: TransactionI;
 }
 
 /* todo record lesson learnt:
@@ -31,62 +31,63 @@ interface PropsI {
  * that can cover each with a specific prop,
  * */
 
-export async function addNewTransaction(props: PropsI) {
-  try {
-    await addDoc(
-      collection(db, 'users', currentUser(), props.transactionType),
-      {
-        ...props.transaction,
-        user_id: currentUser(),
-      },
-    );
-    toast.success('Transaction logged');
-  } catch (e) {
-    console.error('Error adding transaction: ', e);
-    toast.error('Error adding transaction');
-  }
+export async function addNewTransaction(props: TransactionPropsI) {
+	try {
+		const res = await addDoc(
+			collection(db, "users", currentUser(), props.transactionType),
+			{
+				...props.transaction,
+				user_id: currentUser(),
+			},
+		);
+		toast.success("Transaction logged");
+		return res;
+	} catch (e) {
+		console.error("Error adding transaction: ", e);
+		toast.error("Error adding transaction");
+	}
 }
 
 export async function getExpenses() {
-  try {
-    let allExpenses: TransactionI[] = [];
-    const querySnapshot = await getDocs(
-      collection(db, 'users', currentUser(), `expenses`),
-    );
-    querySnapshot.forEach((doc) => {
-      const data = doc.data() as TransactionI; // Type assertion
-      allExpenses.push(data);
-    });
-    return allExpenses;
-  } catch (e) {
-    console.error(
-      'Error retrieving expenses: ',
-      e instanceof Error ? e.message : e,
-    );
-  }
+	try {
+		let allExpenses: TransactionI[] = [];
+		const querySnapshot = await getDocs(
+			collection(db, "users", currentUser(), `expenses`),
+		);
+		querySnapshot.forEach((doc) => {
+			const data = doc.data() as TransactionI; // Type assertion
+			allExpenses.push(data);
+		});
+		return allExpenses;
+	} catch (e) {
+		console.error(
+			"Error retrieving expenses: ",
+			e instanceof Error ? e.message : e,
+		);
+	}
 }
 
 // TODO update all the below when implementation is needed
 interface UpdateExpensePropsI {
-  db: any;
-  expenseId: string;
-  key: string;
-  updatedValue: any;
+	db: any;
+	expenseId: string;
+	key: string;
+	updatedValue: any;
 }
 
 export async function updateExpense({
-  expenseId,
-  key,
-  updatedValue,
+	expenseId,
+	key,
+	updatedValue,
 }: UpdateExpensePropsI) {
-  try {
-    const updatedRef = doc(db, 'expenses', expenseId);
-    await updateDoc(updatedRef, {
-      [key]: updatedValue,
-    });
-    toast.success('Expense updated');
-  } catch (e) {
-    console.error('Error updating expense: ', e);
-    toast.error('Error updating expense');
-  }
+	try {
+		const updatedRef = doc(db, "expenses", expenseId);
+		await updateDoc(updatedRef, {
+			[key]: updatedValue,
+		});
+		toast.success("Expense updated");
+	} catch (e) {
+		console.error("Error updating expense: ", e);
+		toast.error("Error updating expense");
+	}
 }
