523d7380b620c3e373785aabed285163252afe79
GiuliaPassoni <kirakira95@msn.com>
2024-12-28 15:04:09 +0100

    * chore: remove unused component.

--- src/components/atoms/StyledButton/index.tsx
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-// TODO this works! Apply app-wide!
-export default function StyledButton(props: any) {
-	return (
-		<button onClick={props.onClick} class="styled-button">
-			{props.text}
-		</button>
-	);
-}
