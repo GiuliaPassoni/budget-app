e4e604543cd26a9f366b211329cf6db488f357a8
GiuliaPassoni <kirakira95@msn.com>
2024-12-28 15:04:32 +0100

    * refactor: update button compnent.

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,25 +1,25 @@
-// TODO check out https://tailwindcss.com/docs/reusing-styles for reusability
+import "./style.css";
+import { JSX } from "solid-js";
+
 interface IProps {
 	onClick(): void;
 	text: string;
+	children?: JSX.Element;
 	disabled?: boolean;
 }
 
 export default function Button(props: IProps) {
 	const text = () => props.text;
+	const children = () => props.children ?? null;
 	const disabled = () => props.disabled;
 	// const handleClick = props.onClick;
 	return (
 		<button
 			onClick={props.onClick} //todo check if this still works, else, revert to props.onClick
-			class="inline-flex items-center px-3 py-2
-			text-sm font-medium text-center text-white
-			bg-blue-700 dark:bg-blue-600
-			rounded-lg
-			hover:bg-blue-800 dark:hover:bg-blue-700
-			focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+			class="button-style"
 		>
 			{text()}
+			{children() ? children() : null}
 		</button>
 	);
 }
