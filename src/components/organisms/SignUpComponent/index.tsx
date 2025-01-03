c71a3834ce42555ff87a451e6af17bfb798634ad
GiuliaPassoni <kirakira95@msn.com>
2025-01-03 20:51:42 +0100

    * feat: add form validation to signup/login.

--- src/components/organisms/SignUpComponent/index.tsx
+++ src/components/organisms/SignUpComponent/index.tsx
@@ -1,27 +1,23 @@
 import { Toaster } from "solid-toast";
-import { useNavigate } from "@solidjs/router";
-import { handleSignIn, handleSignUp } from "~/helpers/auth_helpers";
-import addUser from "~/helpers/db_helpers";
-
 import "./style.css";
-import { createStore } from "solid-js/store";
-import { FormI, InputI } from "~/helpers/types";
+import { RegisterForm } from "~/helpers/forms/formTypes";
+import {
+	emailValidator,
+	inputLengthValidator,
+	matchingPasswords,
+	requiredField,
+} from "~/helpers/forms/validators";
+import FormError from "~/components/atoms/FormError";
 import useForm from "~/hooks/useForm";
 
-interface UserI {
-	id?: string;
-	email: string;
-	password: string;
-	isLoggedIn?: boolean;
-}
-
 export default function SignUpComponent() {
-	const { handleInput, handleSubmitSignUp, handleSubmitSignIn } = useForm({
-		name: "",
-		email: "",
-		password: "",
-		confirmPassword: "",
-	});
+	const { form, handleInput, validate, handleSubmit, errors } =
+		useForm<RegisterForm>({
+			name: "",
+			email: "",
+			password: "",
+			confirmPassword: "",
+		});
 
 	return (
 		<>
@@ -29,50 +25,70 @@ export default function SignUpComponent() {
 				<div class="entry">
 					<label for="name">Your name</label>
 					<input
+						use:validate={[requiredField, inputLengthValidator]}
 						type="name"
 						name="name"
 						id="name"
 						placeholder="Elizabeth I"
 						onInput={handleInput}
 					/>
+					<FormError>{errors["name"]}</FormError>
 				</div>
 				<div class="entry">
 					<label for="email">Your email</label>
 					<input
+						use:validate={[requiredField, emailValidator]}
 						type="email"
 						name="email"
 						id="email"
-						placeholder="name@flowbite.com"
+						placeholder="name@budgetapp.com"
 						required
 						onInput={handleInput}
 					/>
+					<FormError>{errors["email"]}</FormError>
 				</div>
 				<div class="entry">
 					<label for="password">Your password</label>
 					<input
+						use:validate={[
+							requiredField,
+							(el) => inputLengthValidator(el, 7, 16),
+						]}
 						type="password"
 						name="password"
 						id="password"
 						required
 						onInput={handleInput}
 					/>
+					<FormError>{errors["password"]}</FormError>
 				</div>
 				<div class="entry">
 					<label for="confirmPassword">Confirm password</label>
 					<input
+						use:validate={[
+							requiredField,
+							(el) => matchingPasswords(el, form.password),
+						]}
 						type="password"
 						name="confirmPassword"
 						id="confirmPassword"
 						required
 						onInput={handleInput}
 					/>
+					<FormError>{errors["confirmPassword"]}</FormError>
 				</div>
 				<div class="submit-buttons">
-					<button type="submit" onClick={(e) => handleSubmitSignUp(e)}>
+					<button
+						type="submit"
+						onClick={(e) => handleSubmit({ e, type: "signup" })}
+					>
 						Sign up
 					</button>
 					{/*//todo only render if user already exists after validation checks*/}
-					<button type="submit" onClick={(e) => handleSubmitSignIn(e)}>
+					<button
+						type="submit"
+						onClick={(e) => handleSubmit({ e, type: "login" })}
+					>
 						Log in
 					</button>
 				</div>
--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,38 +1,116 @@
-import { createStore } from "solid-js/store";
+import { createStore, produce } from "solid-js/store";
 import { useNavigate } from "@solidjs/router";
 import { handleSignIn, handleSignUp } from "~/helpers/auth_helpers";
 import addUser from "~/helpers/db_helpers";
-import { ButtonI, FormI, InputI } from "~/helpers/types";
+import {
+	ButtonI,
+	InputI,
+	FormDataType,
+	SubmitCallback,
+	FormErrors,
+	ValidatorType,
+	ValidatorFieldConfig,
+} from "~/helpers/forms/formTypes";
+import JSX from "solid-js";
 
-export default function useForm(initialForm: FormI) {
-	const [form, setForm] = createStore<FormI>(initialForm);
+// Add custom directive
+declare module "solid-js/jsx-runtime" {
+	namespace JSX {
+		interface Directives {
+			validate: ValidatorType[];
+		}
+	}
+}
+
+interface PropsI<T extends FormDataType> {
+	e: ButtonI;
+	type: "signup" | "login";
+	callback?: SubmitCallback<T>;
+}
+
+// todo reuse this form for the settings
+export default function useForm<T extends FormDataType>(initialForm: T) {
+	const [form, setForm] = createStore(initialForm);
+	const [errors, setErrors] = createStore<FormErrors>({});
+
+	const validatorFields: { [key: string]: ValidatorFieldConfig } = {};
 
 	const navigate = useNavigate();
 
+	function isValid() {
+		const keys = Object.keys(errors);
+		if (keys.length === 0) return false; //no input was validated, hence form isn't valid
+
+		// if no error has value, the form is valid
+		return !keys.some((errKey) => {
+			return errors[errKey].length > 0;
+		});
+	}
+
 	function handleInput(e: InputI) {
 		const { name, value } = e.currentTarget;
-		setForm(name as keyof FormI, value);
-	}
-	//todo instead of a separate login component, adjust this according to need?
-
-	async function handleSubmitSignUp(e: ButtonI) {
-		e.preventDefault();
-		const user = await handleSignUp({
-			email: form.email,
-			password: form.password,
-		});
-		await addUser({ user });
-		navigate("/auth/overview");
+		setForm(name as any, value as any);
 	}
 
-	async function handleSubmitSignIn(e: ButtonI) {
-		e.preventDefault();
-		handleSignIn({ email: form.email, password: form.password });
-		navigate("/auth/overview");
+	function validate(
+		element: HTMLInputElement,
+		accessor: JSX.Accessor<ValidatorType[]>,
+	) {
+		const validators = accessor() || [];
+
+		let config: ValidatorFieldConfig;
+		validatorFields[element.name] = config = { element, validators };
+
+		element.onblur = () => {
+			checkValidity(config);
+		};
 	}
+
+	function checkValidity({ element, validators }: ValidatorFieldConfig) {
+		setErrors(element.name, []);
+		console.debug("checkValidity", element.name, errors[element.name]);
+
+		for (const validator of validators) {
+			const errorMessage = validator(element);
+			console.log("errorMessage", errorMessage);
+			if (!!errorMessage) {
+				setErrors(
+					produce((errors) => {
+						errors[element.name].push(errorMessage);
+					}),
+				);
+			}
+		}
+	}
+
+	async function handleSubmit({ e, type, callback }: PropsI<FormDataType>) {
+		e.preventDefault();
+		for (const field in validatorFields) {
+			const config = validatorFields[field];
+			checkValidity(config);
+		}
+		if (isValid()) {
+			if (callback) {
+				return callback(form);
+			}
+			if (type === "signup") {
+				const user = await handleSignUp({
+					email: form.email,
+					password: form.password,
+				});
+				await addUser({ user });
+			} else {
+				await handleSignIn({ email: form.email, password: form.password });
+			}
+			navigate("/auth/overview");
+		}
+	}
+
 	return {
+		form,
 		handleInput,
-		handleSubmitSignUp,
-		handleSubmitSignIn,
+		validate,
+		handleSubmit,
+		errors,
 	};
 }
