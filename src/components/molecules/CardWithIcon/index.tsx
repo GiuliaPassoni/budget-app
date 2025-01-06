77a3d310922f6039fa97b4fbfe99badf88762be4
giuliapassoni <kirakira95@msn.com>
2025-01-06 19:44:51 +0100

    * style: card with icon.

--- src/components/molecules/CardWithIcon/index.tsx
+++ src/components/molecules/CardWithIcon/index.tsx
@@ -1,5 +1,6 @@
 import { Show } from "solid-js";
 import { pastelColors } from "~/helpers/colour_helpers";
+import "./style.css";
 
 interface IProps {
 	icon: any;
@@ -18,26 +19,11 @@ export default function CardWithIcon({
 }: IProps) {
 	const bgColour = pastelColors.find((i) => i.name == colour);
 	return (
-		<a
-			href="#"
-			onClick={handleClick}
-			class={`
-			col-span-4
-			block
-			max-w-100px
-			m-2
-			p-2 px-auto
-			${bgColour?.colourClass} border border-gray-200
-			rounded-lg
-			shadow dark:bg-gray-800 dark:border-gray-700
-			`}
-		>
-			<div class="block mx-auto w-fit">{icon}</div>
-			<h4 class="my-1 mx-auto w-fit text-gray-900 dark:text-white">{title}</h4>
+		<a href="#" onClick={handleClick} class="card">
+			<div class={`icon-container ${bgColour?.colourClass}`}>{icon}</div>
+			<h4 class="description">{title}</h4>
 			<Show when={children}>
-				<p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
-					{children}
-				</p>
+				<p class="children">{children}</p>
 			</Show>
 		</a>
 	);
