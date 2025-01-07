44beab82a0651c54a715cf9731229ab2367f30e8
giuliapassoni <kirakira95@msn.com>
2025-01-07 09:17:49 +0100

    * feat: add plus icon.

---
+++ src/components/atoms/icons/PlusIcon.tsx
@@ -0,0 +1,21 @@
+import "./style.css";
+
+export default function PlusIcon() {
+	return (
+		<svg
+			class="icon-all"
+			width="24"
+			height="24"
+			fill="none"
+			viewBox="0 0 24 24"
+		>
+			<path
+				stroke="currentColor"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M5 12h14m-7 7V5"
+			/>
+		</svg>
+	);
+}
--- src/components/atoms/icons/helpers.ts
+++ src/components/atoms/icons/helpers.ts
@@ -1,10 +1,12 @@
-import { JSX } from 'solid-js';
-import MoonIcon from '~/components/atoms/icons/MoonIcon';
-import SunIcon from '~/components/atoms/icons/SunIcon';
-import StarIcon from '~/components/atoms/icons/StarIcon';
+import { JSX } from "solid-js";
+import MoonIcon from "~/components/atoms/icons/MoonIcon";
+import SunIcon from "~/components/atoms/icons/SunIcon";
+import StarIcon from "~/components/atoms/icons/StarIcon";
+import PlusIcon from "~/components/atoms/icons/PlusIcon";
 
 export const iconMap: { [key: string]: () => JSX.Element } = {
-  moon: MoonIcon,
-  sun: SunIcon,
-  star: StarIcon,
+	moon: MoonIcon,
+	sun: SunIcon,
+	star: StarIcon,
+	plus: PlusIcon,
 };
