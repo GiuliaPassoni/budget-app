2b0f2b03882b72470b7d42fa55b7845f2c63bc67
giuliapassoni <kirakira95@msn.com>
2025-01-07 09:19:59 +0100

    * refactor: add custom styling to button (wip).

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -4,19 +4,21 @@ import { JSX } from "solid-js";
 interface IProps {
 	onClick(): void;
 	text: string;
+	styleClass?: string; //todo make it work better or add primary and secondary styling
 	children?: JSX.Element;
 	disabled?: boolean;
 }
 
 export default function Button(props: IProps) {
 	const text = () => props.text;
+	const classParser = String(props.styleClass);
 	const children = () => props.children ?? null;
 	const disabled = () => props.disabled;
 	// const handleClick = props.onClick;
 	return (
 		<button
 			onClick={props.onClick} //todo check if this still works, else, revert to props.onClick
-			class="button-style"
+			class={`button-style ${classParser}`}
 		>
 			{text()}
 			{children() ? children() : null}
