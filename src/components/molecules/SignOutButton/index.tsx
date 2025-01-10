5b66c1cbd7b2ca83574ae93c9ccc157a53738afd
giuliapassoni <kirakira95@msn.com>
2025-01-10 19:40:01 +0100

    * refactor: signout button (wip).

--- src/components/molecules/SignOutButton/index.tsx
+++ src/components/molecules/SignOutButton/index.tsx
@@ -1,29 +1,33 @@
-import { Toaster } from 'solid-toast';
-import Avatar from '~/components/atoms/Avatar';
-import { Show } from 'solid-js';
-import { useNavigate } from '@solidjs/router';
-import { handleLogOut } from '~/helpers/auth_helpers';
-import { auth } from '~/firebase';
-import Button from '~/components/atoms/Button';
+import { Toaster } from "solid-toast";
+import Avatar from "~/components/atoms/Avatar";
+import { Show } from "solid-js";
+import { useNavigate } from "@solidjs/router";
+import { handleLogOut } from "~/helpers/auth_helpers";
+import { auth } from "~/firebase";
+import Button from "~/components/atoms/Button";
 
+// todo refactor with https://flowbite.com/docs/components/dropdowns/#user-avatar
 export default function SignOutButton() {
-  const user = auth.currentUser;
-  const navigate = useNavigate();
-  return (
-    <Show when={user}>
-      <Avatar
-        email={user?.email}
-        name={user?.displayName}
-        pic={user?.photoURL}
-      />
-      <Button
-        text="Sign out button"
-        onClick={() => {
-          navigate('/overview');
-          handleLogOut({ userName: user?.email });
-        }}
-      ></Button>
-      <Toaster />
-    </Show>
-  );
+	const user = auth.currentUser;
+	const navigate = useNavigate();
+	return (
+		<Show when={user}>
+			<Button
+				styleClass="secondary"
+				text="Sign out button"
+				leftIcon={
+					<Avatar
+						email={user?.email}
+						name={user?.displayName}
+						pic={user?.photoURL}
+					/>
+				}
+				onClick={() => {
+					navigate("/overview");
+					handleLogOut({ userName: user?.email });
+				}}
+			/>
+			<Toaster />
+		</Show>
+	);
 }
