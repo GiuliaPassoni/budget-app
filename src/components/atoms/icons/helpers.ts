c2643e675e27f87d969d4f984acb92e06637beb5
giuliapassoni <kirakira95@msn.com>
2025-01-10 08:42:35 +0100

    * feat: add edit icon.

--- src/components/atoms/icons/helpers.ts
+++ src/components/atoms/icons/helpers.ts
@@ -3,10 +3,12 @@ import MoonIcon from "~/components/atoms/icons/MoonIcon";
 import SunIcon from "~/components/atoms/icons/SunIcon";
 import StarIcon from "~/components/atoms/icons/StarIcon";
 import PlusIcon from "~/components/atoms/icons/PlusIcon";
+import EditIcon from "~/components/atoms/icons/EditIcon";
 
 export const iconMap: { [key: string]: () => JSX.Element } = {
 	moon: MoonIcon,
 	sun: SunIcon,
 	star: StarIcon,
 	plus: PlusIcon,
+	edit: EditIcon,
 };
