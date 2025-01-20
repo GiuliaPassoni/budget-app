66256c55adaac3f9512d2a0d8c71c79cd87f1196
giuliapassoni <kirakira95@msn.com>
2025-01-20 19:22:50 +0100

    * refactor: categories page and modal invokation.

--- src/components/organisms/CategoriesComponent/index.tsx
+++ src/components/organisms/CategoriesComponent/index.tsx
@@ -1,53 +1,57 @@
-import StarIcon from "~/components/atoms/icons/StarIcon";
-import MoonIcon from "~/components/atoms/icons/MoonIcon";
-import SunIcon from "~/components/atoms/icons/SunIcon";
-import Card from "~/components/atoms/Card";
 import CardWithIcon from "~/components/molecules/CardWithIcon";
-import PlusIconButton from "~/components/atoms/PlusIconButton";
-import { createSignal } from "solid-js";
+import { createSignal, For } from "solid-js";
 import AddCategoryModal from "~/components/molecules/AddCategoryModal";
 import { Toaster } from "solid-toast";
 import MainLayout from "~/components/organisms/MainLayout";
-
-import "./style.css";
-import PlusIcon from "~/components/atoms/icons/PlusIcon";
+import { useFirebaseCollection } from "~/hooks/useFirebaseCollection";
+import { CategoryI, CategoryWithId } from "~/helpers/types";
+import { currentUser, db } from "~/firebase";
+import { iconMap } from "~/components/atoms/icons/helpers";
+import styles from "./style.module.css";
 
 export default function CategoriesComponent() {
 	const [showModal, setShowModal] = createSignal(false);
+
+	const { data: categories } = useFirebaseCollection<CategoryI, CategoryWithId>(
+		{
+			db,
+			collectionPath: () => {
+				const userId = currentUser();
+				return userId ? [`users/${currentUser()}/categories`] : undefined;
+			},
+		},
+	);
+
 	return (
 		<MainLayout title="Categories">
-			<div class="container">
-				<Card>Tab with income categories</Card>
-				<Card>Tab with expense categories</Card>
-				<Card>Tab with investing categories</Card>
-				{/*todo parse category and make icon card*/}
-				<CardWithIcon colour="yellow" icon={<StarIcon />} title="Star income" />
-				<CardWithIcon
-					colour="blue-500"
-					icon={<MoonIcon />}
-					title="Moon income"
-				/>
-				<CardWithIcon
-					colour="green-300"
-					icon={<SunIcon />}
-					title="Sun income"
-				/>
-				<PlusIconButton
-					variant="secondary"
-					type="submit"
-					handleClick={() => {
-						setShowModal(true);
-					}}
-					title="Add category"
-					leftIcon={<PlusIcon />}
-				/>
-				<AddCategoryModal
-					showModal={showModal()}
-					handleClose={() => setShowModal(false)}
-					onSubmit={() => setShowModal(false)}
-				/>
-				<Toaster />
+			<div class={styles.container}>
+				<div class={`${styles.categoryGrid}`}>
+					<For each={categories()}>
+						{(i) => (
+							<CardWithIcon
+								colour={i.colour}
+								title={i.name}
+								icon={i.iconName ? iconMap[i.iconName]?.() : ""}
+								handleClick={() => {}}
+							/>
+						)}
+					</For>
+					<CardWithIcon
+						colour="gray"
+						title="More"
+						icon={iconMap["plus"]}
+						handleClick={() => {
+							setShowModal(true);
+						}}
+					/>
+				</div>
 			</div>
+			<AddCategoryModal
+				showModal={showModal()}
+				handleClose={() => setShowModal(false)}
+				onSubmit={() => setShowModal(false)}
+			/>
+			<Toaster />
 		</MainLayout>
 	);
 }
