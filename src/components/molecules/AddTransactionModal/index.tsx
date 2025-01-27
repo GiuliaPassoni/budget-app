700728a7b891be6d4d725fa2372382b5ad7ba0e6
giuliapassoni <kirakira95@msn.com>
2025-01-27 08:43:47 +0100

    * refactor: reuse for editing.

--- src/components/molecules/AddTransactionModal/index.tsx
+++ src/components/molecules/AddTransactionModal/index.tsx
@@ -1,15 +1,19 @@
-import { createMemo, createSignal, For } from "solid-js";
+import { createMemo, createSignal, For, onMount, Show } from "solid-js";
 import { toast, Toaster } from "solid-toast";
 import {
-	addNewTransaction,
 	TransactionI,
+	TransactionWithId,
 } from "~/helpers/expenses_api_helpers";
 import allCurrencies from "~/helpers/mock_values_helpers";
 import PlusIconButton from "~/components/atoms/PlusIconButton";
 import CardWithIcon from "~/components/molecules/CardWithIcon";
 import { CategoryI, CategoryWithId, TransactionType } from "~/helpers/types";
-import { getCategories } from "~/helpers/categories_api_helpers";
-import { currentUser, db } from "~/firebase";
+import {
+	deleteItem,
+	getItemByIdOrName,
+	updateItem,
+} from "~/helpers/categories_api_helpers";
+import { currentUser } from "~/firebase";
 import AddCategoryModal from "~/components/molecules/AddCategoryModal";
 import { iconMap } from "~/components/atoms/icons/helpers";
 import Modal from "~/components/molecules/Modal";
@@ -19,9 +23,14 @@ import Datepicker from "~/components/atoms/Datepicker";
 import Button from "~/components/atoms/Button";
 import { createStore } from "solid-js/store";
 import { useFirebaseCollection } from "~/hooks/useFirebaseCollection";
+import PlusIcon from "~/components/atoms/icons/PlusIcon";
+import BinIcon from "~/components/atoms/icons/BinIcon";
+import LoadingSpinner from "~/components/atoms/LoadingSpinner";
 
 interface ModalProps {
 	showModal: boolean;
+	isEditTransactionModal: boolean;
+	transactionToEdit?: TransactionI;
 	handleClose: () => void;
 	onSubmit: () => void;
 }
@@ -41,20 +50,56 @@ const isSameDay = (date1: Date, date2: Date) => {
 
 export default function AddTransactionModal(props: ModalProps) {
 	const showModal = () => props.showModal;
-
+	const [loading, setIsLoading] = createSignal<boolean>(false);
+	const isEditTransactionModal = () => props.isEditTransactionModal;
+	const [showCategModal, setShowCategModal] = createSignal<boolean>(false);
 	const [date, setDate] = createSignal<Date>(new Date());
 	const [transaction, setTransaction] = createStore<Pick<TransactionI, any>>({
 		type: "expenses",
 		amount: 0,
 		currency: "EUR",
-		exchange: 1,
-		category: "",
+		exchange_to_default: 1,
+		ctg_name: "",
 		notes: "",
+		// 	date is separate due to datepicker config
 	});
-	const [showCategModal, setShowCategModal] = createSignal<boolean>(false);
+	const [editCategory, setEditCategory] = createSignal<CategoryI>();
+
+	onMount(async () => {
+		if (isEditTransactionModal() && props.transactionToEdit) {
+			setIsLoading(true);
+			setTransaction(props.transactionToEdit);
+			try {
+				const ctgName = transaction.ctg_name;
+				const categ = await getItemByIdOrName({
+					dbName: "categories",
+					name: ctgName,
+				});
+				if (categ) {
+					setEditCategory(categ.data);
+				} else {
+					console.warn("No category found");
+				}
+			} catch (err) {
+				console.error("Failed to load category:", err);
+			} finally {
+				setIsLoading(false);
+			}
+		} else {
+			// Reset to default values for "Add" mode
+			setTransaction({
+				type: "expenses",
+				amount: 0,
+				currency: "EUR",
+				exchange_to_default: 1,
+				ctg_name: "",
+				notes: "",
+			});
+		}
+	});
+
 	function handleTabClick(prop: TransactionType) {
-		setTransaction("type", prop); //todo is this necessary, since the db is already the type?
-		test();
+		setTransaction("type", prop); //todo replace with separate signal?
 	}
 
 	const dates = createMemo(() => {
@@ -90,65 +135,61 @@ export default function AddTransactionModal(props: ModalProps) {
 		);
 	};
 
-	async function test() {
-		return await getCategories();
-	}
-
-	// todo refactor method to be included in hook and use as below
-	// const { add: addTransaction } = useFirebaseCollection<
-	// 	TransactionI,
-	// 	TransactionWithId
-	// >({
-	// 	db,
-	// 	collectionPath: () => {
-	// 		const userId = currentUser();
-	// 		return userId ? [`users/${currentUser()}/transactions`] : undefined;
-	// 	},
-	// });
-
 	async function handleSubmit() {
-		// TODO refactor the below to be all props of one single signal-object
-		const { amount, currency, exchange, category, type, notes } = transaction;
-		if (amount && currency && exchange && category) {
+		const { amount, currency, exchange_to_default, ctg_name, type, notes } =
+			transaction;
+		if (transaction) {
 			const newTransaction = {
 				amount,
 				currency,
-				exchange_to_default: exchange,
+				exchange_to_default,
 				notes,
 				date: new Date(),
-				ctg_name: category,
+				ctg_name,
 				type,
 			};
 			if (!type) {
 				toast.error("Please specify transaction type");
-			} else {
+			}
+			try {
+				const newTransactionId = await addTransaction(newTransaction);
 				if (props.onSubmit) {
-					props.onSubmit;
+					props.onSubmit();
 				}
-				await addNewTransaction({
-					transactionType: type,
-					transaction: newTransaction,
-				});
 				props.handleClose();
+			} catch (error) {
+				toast.error(`Failed to add transaction: ${error.message}`);
 			}
 		} else {
 			toast.error("Missing input");
 		}
 	}
 
-	// // todo hanlde tags in database. for now, use dummy
-	// const dummyTags = ["flights", "Lidl", "train", "coffee"];
-	// const [tags, setTags] = createSignal(dummyTags);
+	const { add: addTransaction } = useFirebaseCollection<
+		TransactionI,
+		TransactionWithId
+	>({ collectionPath: () => [`users/${currentUser()}/${transaction.type}`] });
 
 	const { data: categories } = useFirebaseCollection<CategoryI, CategoryWithId>(
 		{
-			db,
-			collectionPath: () => {
-				const userId = currentUser();
-				return userId ? [`users/${currentUser()}/categories`] : undefined;
-			},
+			collectionPath: () => [`users/${currentUser()}/categories`],
 		},
 	);
+
+	async function handleEdit() {
+		return await updateItem({
+			dbName: transaction.type,
+			itemRef: transaction.id,
+			updatedValue: transaction,
+		});
+	}
+
+	async function handleDelete() {
+		return await deleteItem({
+			dbName: transaction.type,
+			itemRef: transaction.id,
+		});
+	}
 
 	return (
 		<div>
@@ -167,6 +208,7 @@ export default function AddTransactionModal(props: ModalProps) {
 							required={true}
 							id="typeSelect"
 							class={styles.formElements}
+							value={transaction?.type ?? "expenses"}
 						>
 							<For each={["expenses", "income", "investments"]}>
 								{(i) => <option value={i}>{i}</option>}
@@ -176,6 +218,7 @@ export default function AddTransactionModal(props: ModalProps) {
 					<span>
 						<label for="price">Amount</label>
 						<input
+							value={isEditTransactionModal() ? transaction?.amount : 0}
 							id="priceInput"
 							type="number"
 							name="price"
@@ -197,6 +240,7 @@ export default function AddTransactionModal(props: ModalProps) {
 							required={true}
 							id="currencySelect"
 							class={styles.formElements}
+							value={transaction.currency ?? "EUR"} //TODO fix default currency to user's
 						>
 							<For each={allCurrencies}>
 								{(i) => (
@@ -210,31 +254,47 @@ export default function AddTransactionModal(props: ModalProps) {
 				</div>
 				<div class={styles.categoryContainer}>
 					<label for="category">Category</label>
-					{/*//todo fix scrollbar not showing. The issue is the OS.*/}
-					<div class={`${styles.categoryGrid} scrollbar-always`}>
-						<For each={categories()}>
-							{(i) => (
-								<CardWithIcon
-									selected={i.name === transaction.category}
-									colour={i.colour}
-									title={i.name}
-									icon={i.iconName ? iconMap[i.iconName]?.() : ""}
-									handleClick={() => {
-										setTransaction("category", i.name);
-									}}
-								/>
-							)}
-						</For>
-						{/*todo connect category modal, or handle the flow otherwise*/}
-						<CardWithIcon
-							colour="gray"
-							title="More"
-							icon={iconMap["plus"]}
-							handleClick={() => {
-								setShowCategModal(true);
-							}}
-						/>
-					</div>
+					<Show when={!loading()} fallback={<LoadingSpinner />}>
+						<div class={`${styles.categoryGrid} scrollbar-always`}>
+							<For
+								each={
+									isEditTransactionModal()
+										? [
+												// Move the matching category to the front
+												...categories().filter(
+													(cat) => cat.name === transaction.ctg_name,
+												),
+												// Include the rest of the categories
+												...categories().filter(
+													(cat) => cat.name !== transaction.ctg_name,
+												),
+											]
+										: categories() // Use the original list if not in edit mode
+								}
+							>
+								{(i) => (
+									<CardWithIcon
+										selected={i.name === transaction.ctg_name}
+										colour={i.colour}
+										title={i.name}
+										icon={i.iconName ? iconMap[i.iconName]?.() : ""}
+										handleClick={() => {
+											setTransaction("ctg_name", i.name);
+										}}
+									/>
+								)}
+							</For>
+							{/*todo connect category modal, or handle the flow otherwise*/}
+							<CardWithIcon
+								colour="gray"
+								title="Add new category"
+								icon={iconMap["plus"]}
+								handleClick={() => {
+									setShowCategModal(true);
+								}}
+							/>
+						</div>
+					</Show>
 				</div>
 				<div>
 					<label for="date">Transaction Date</label>
@@ -243,7 +303,7 @@ export default function AddTransactionModal(props: ModalProps) {
 							{(item) => (
 								<Button
 									type="button"
-									styleClass={`secondary w-1/4 ${isButtonSelected(item.date) ? "selected" : ""}`}
+									styleClass={`secondary ${isButtonSelected(item.date) ? "selected" : ""}`}
 									onClick={() => setDate(item.date)}
 								>
 									<h3>
@@ -288,18 +348,49 @@ export default function AddTransactionModal(props: ModalProps) {
 					></textarea>
 				</div>
 				{/*tooltip doesn't work, may be hiding behind modal*/}
-				<PlusIconButton
-					type="submit"
-					variant="primary"
-					handleClick={async (e: Event) => {
-						e.preventDefault();
-						await handleSubmit();
-					}}
-					title="Add new transaction"
-				/>
+				<Show
+					when={isEditTransactionModal()}
+					fallback={
+						<PlusIconButton
+							type="submit"
+							variant="primary"
+							handleClick={async (e: Event) => {
+								e.preventDefault();
+								await handleSubmit();
+							}}
+							title="Add new transaction"
+						/>
+					}
+				>
+					<div class="grid grid-cols-2 gap-10 mt-8">
+						<Button
+							styleClass="primary edit-submit mx-auto"
+							leftIcon={<PlusIcon />}
+							type="submit"
+							onClick={async () => {
+								await handleEdit();
+								props.handleClose();
+							}}
+							text="Save edit"
+						/>
+
+						<Button
+							leftIcon={<BinIcon />}
+							type="submit"
+							styleClass="danger mx-auto"
+							onClick={async () => {
+								await handleDelete();
+								props.handleClose();
+							}}
+						>
+							Delete Transaction
+						</Button>
+					</div>
+				</Show>
 			</Modal>
 			{/*todo try to pass the mutation as a prop from here, since scope is available here - hopefully it maintains the reference */}
 			<AddCategoryModal
+				isEditCategoryModal={false}
 				showModal={showCategModal()}
 				handleClose={() => setShowCategModal(false)}
 				onSubmit={() => setShowCategModal(false)}
