7749ff90313674515bec60ad76eb694dbe8c6da9
giuliapassoni <kirakira95@msn.com>
2025-01-31 08:34:08 +0100

    * refactor: loading states in mainLayout.

--- src/components/organisms/MainLayout/index.tsx
+++ src/components/organisms/MainLayout/index.tsx
@@ -1,8 +1,9 @@
-import { createEffect, JSX } from "solid-js";
+import { createEffect, JSX, Match, Switch } from "solid-js";
 
 import "./style.css";
 import { useAuthState } from "~/services/provider/auth";
 import { useNavigate } from "@solidjs/router";
+import LoadingSpinner from "~/components/atoms/LoadingSpinner";
 
 interface PropsI {
 	title: JSX.Element | string;
@@ -10,22 +11,34 @@ interface PropsI {
 }
 
 export default function MainLayout(props: PropsI) {
-	const authState = useAuthState();
+	// const authState = useAuthState();
 	const navigate = useNavigate();
+	const { user, loading, isAuthenticated } = useAuthState();
 
 	createEffect(() => {
-		if (authState.loading) return; // Wait for auth initialization
-		if (!authState?.isAuthenticated) {
+		if (loading) return;
+		if (!isAuthenticated) {
 			navigate("/login", { replace: true });
 		}
 	});
 
 	return (
-		<div class="main-layout">
-			<div class="layout-center">
-				<div class="title">{props.title}</div>
-				<div class="main-content">{props.children}</div>
-			</div>
-		</div>
+		<Switch>
+			<Match when={loading}>
+				<LoadingSpinner />
+				<div>Loading...</div>
+			</Match>
+			<Match when={!loading && !user}>
+				<div>Please log in to continue.</div>
+			</Match>
+			<Match when={!loading && user}>
+				<div class="main-layout">
+					<div class="layout-center">
+						<div class="title">{props.title}</div>
+						<div class="main-content">{props.children}</div>
+					</div>
+				</div>
+			</Match>
+		</Switch>
 	);
 }
