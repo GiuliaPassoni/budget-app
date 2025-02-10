de7ed9c56c150e0b94d1dccd4cb152e2448c068d
giuliapassoni <kirakira95@msn.com>
2025-02-10 09:24:06 +0100

    * feat: add date sorting in table.

--- src/components/molecules/Table/index.tsx
+++ src/components/molecules/Table/index.tsx
@@ -26,21 +26,45 @@ function firestoreTimestampToDate(timestamp: {
 }
 
 export default function Table(props: TableProps) {
-	const headers = [
-		"Transaction amount",
-		"Currency",
-		"Date",
-		"Category",
-		"Notes",
-	];
-
-	const [showEditTransactionModal, setShowEditTransactionModal] =
-		createSignal(false);
+	const [sortOrder, setSortOrder] = createSignal<"asc" | "desc">("desc");
+	const [sortedArray, setSortedArray] = createSignal<TransactionI[]>(
+		props.array || [],
+	);
 	const [selectedTransaction, setSelectedTransaction] = createSignal<
 		TransactionI | undefined
 	>(undefined);
+	const [showEditTransactionModal, setShowEditTransactionModal] =
+		createSignal(false);
 
-	// todo add filtering
+	const sortByDate = () => {
+		const newSortOrder = sortOrder() === "asc" ? "desc" : "asc";
+		setSortOrder(newSortOrder);
+
+		const sorted = [...sortedArray()].sort((a, b) => {
+			const dateA = firestoreTimestampToDate(a.date).getTime();
+			const dateB = firestoreTimestampToDate(b.date).getTime();
+			return newSortOrder === "asc" ? dateA - dateB : dateB - dateA;
+		});
+
+		setSortedArray(sorted);
+	};
+
+	const headers: Header[] = [
+		{
+			headerName: "Transaction amount",
+			styleClass: "header-left",
+		},
+		{ headerName: "Currency" },
+		{
+			headerName: "Date",
+			styleClass: "px-6 py-3",
+			isSortable: true,
+			sortFn: sortByDate,
+		},
+		{ headerName: "Category" },
+		{ headerName: "Notes" },
+		{ headerName: "Action", styleClass: "header-right" },
+	];
 
 	return (
 		<div>
@@ -56,7 +80,7 @@ export default function Table(props: TableProps) {
 							</tr>
 						) : (
 							<>
-								<For each={props.array}>
+								<For each={sortedArray()}>
 									{(transaction) => (
 										<tr>
 											<td>{transaction.amount}</td>
@@ -105,30 +129,42 @@ export default function Table(props: TableProps) {
 	);
 }
 
+type Header = {
+	headerName: string;
+	styleClass?: string;
+	isSortable?: boolean;
+	sortDirection?: "asc" | "desc";
+	sortFn?: () => void;
+};
+
 interface HeaderPropsI {
-	headers: string[];
+	headers: Header[];
 }
 
 function Header(props: HeaderPropsI) {
+	const headers = () => props.headers;
 	return (
 		<thead>
 			<tr>
-				{/*<For each={props.headers}>*/}
-				{/*	{(i) => <th scope="col">{i}</th>}*/}
-				{/*</For>*/}
-				<th scope="col" class="header-left">
-					Transaction amount
-				</th>
-				<th scope="col">Currency</th>
-				<th scope="col" class="px-6 py-3">
-					Date
-				</th>
-				<th scope="col">Category</th>
-				<th scope="col">Notes</th>
-				<th scope="col" class="header-right">
-					Actions
-				</th>
+				<For each={headers()}>
+					{(i) => (
+						<th scope="col" class={i.styleClass ?? ""}>
+							{i.headerName}{" "}
+							{i.isSortable ? <SortIcon onClick={i.sortFn} /> : ""}
+						</th>
+					)}
+				</For>
 			</tr>
 		</thead>
 	);
 }
+
+const SortIcon = (props: any) => {
+	return (
+		<button onClick={props.onClick}>
+			<svg class="w-3 h-3 ms-1.5" fill="currentColor" viewBox="0 0 24 24">
+				<path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+			</svg>
+		</button>
+	);
+};
