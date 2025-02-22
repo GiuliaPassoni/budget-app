1390c295ef2e40e567e710adf345fb2766c3944d
giuliapassoni <kirakira95@msn.com>
2025-02-22 20:18:57 +0100

    * refactor: remove unused fns, update type.

--- src/helpers/expenses_api_helpers.ts
+++ src/helpers/expenses_api_helpers.ts
@@ -1,12 +1,3 @@
-import {
-	addDoc,
-	collection,
-	doc,
-	getDocs,
-	updateDoc,
-} from "firebase/firestore";
-import { toast } from "solid-toast";
-import { currentUser, db } from "~/firebase";
 import { TransactionType } from "~/helpers/types";
 
 export interface TransactionI {
@@ -18,6 +9,7 @@ export interface TransactionI {
 	notes?: string;
 	date: any;
 	ctg_name: string;
+	ctg_colour?: string;
 	tags?: string[];
 	type?: TransactionType; //todo maybe remove, since the db name is already the type
 }
@@ -26,75 +18,8 @@ export interface TransactionWithId extends TransactionI {
 	id: string;
 }
 
-// TODO add date to type and methods
-export interface TransactionPropsI {
-	transactionType: TransactionType;
-	transaction: TransactionI;
-}
-
 /* todo record lesson learnt:
  * sept 24. I thought initially that there should be a separate method set for expenses, income, and investments.
  * However, I've realised that the method would look the same for all 3 - it makes more sense to have one method, with associated types,
  * that can cover each with a specific prop,
  * */
-
-export async function addNewTransaction(props: TransactionPropsI) {
-	try {
-		const res = await addDoc(
-			collection(db, "users", currentUser(), props.transactionType),
-			{
-				...props.transaction,
-				user_id: currentUser(),
-			},
-		);
-		toast.success("Transaction logged");
-		return res;
-	} catch (e) {
-		console.error("Error adding transaction: ", e);
-		toast.error("Error adding transaction");
-	}
-}
-
-export async function getExpenses() {
-	try {
-		let allExpenses: TransactionI[] = [];
-		const querySnapshot = await getDocs(
-			collection(db, "users", currentUser(), `expenses`),
-		);
-		querySnapshot.forEach((doc) => {
-			const data = doc.data() as TransactionI; // Type assertion
-			allExpenses.push(data);
-		});
-		return allExpenses;
-	} catch (e) {
-		console.error(
-			"Error retrieving expenses: ",
-			e instanceof Error ? e.message : e,
-		);
-	}
-}
-
-// TODO update all the below when implementation is needed
-interface UpdateExpensePropsI {
-	db: any;
-	expenseId: string;
-	key: string;
-	updatedValue: any;
-}
-
-export async function updateExpense({
-	expenseId,
-	key,
-	updatedValue,
-}: UpdateExpensePropsI) {
-	try {
-		const updatedRef = doc(db, "expenses", expenseId);
-		await updateDoc(updatedRef, {
-			[key]: updatedValue,
-		});
-		toast.success("Expense updated");
-	} catch (e) {
-		console.error("Error updating expense: ", e);
-		toast.error("Error updating expense");
-	}
-}
