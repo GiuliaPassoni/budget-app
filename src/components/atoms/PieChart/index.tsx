5f741da20b270c17edf9dc1dcc29a9554fa6bc66
giuliapassoni <kirakira95@msn.com>
2025-02-22 20:36:35 +0100

    * chore: clean up pie chart code.

--- src/components/atoms/PieChart/index.tsx
+++ src/components/atoms/PieChart/index.tsx
@@ -1,6 +1,5 @@
 import * as d3 from "d3";
-import { createEffect, createSignal, For, onMount, Show } from "solid-js";
-import { TransactionI } from "~/helpers/expenses_api_helpers";
+import { createEffect, createSignal, For, Show } from "solid-js";
 
 // borrow from https://stackoverflow.com/questions/1484506/random-color-generator
 function getRandomColor() {
@@ -11,62 +10,6 @@ function getRandomColor() {
 	}
 	return color;
 }
-// sample data returned from db
-const mockTransactionData = [
-	{
-		amount: 0,
-		notes: "",
-		date: {
-			seconds: 1727076694,
-			nanoseconds: 30000000,
-		},
-		user_id: "iZRf4bthopb7vyuxqBmGIsCHbf12",
-		exchange_to_default: 1,
-		currency: "EUR",
-		ctg_name: "Paycheck",
-		// todo add colour from category
-		id: "XD9clGcmczgvrYKiGlbx",
-	},
-	{
-		date: {
-			seconds: 1726646153,
-			nanoseconds: 560000000,
-		},
-		ctg_name: "paycheck",
-		user_id: "iZRf4bthopb7vyuxqBmGIsCHbf12",
-		exchange_to_default: 1,
-		amount: 12,
-		currency: "GBP",
-		notes: "Income",
-		id: "o1o4T9wk5sJLlKdQDBV3",
-	},
-	{
-		exchange_to_default: 1,
-		ctg_name: "paycheck",
-		amount: 100,
-		date: {
-			seconds: 1726644831,
-			nanoseconds: 271000000,
-		},
-		currency: "EUR",
-		notes: "Generous donour",
-		user_id: "iZRf4bthopb7vyuxqBmGIsCHbf12",
-		id: "u9Bfu5EjgrIPlDiHfkNa",
-	},
-	{
-		amount: 10,
-		ctg_name: "Paycheck",
-		exchange_to_default: 1,
-		currency: "EUR",
-		date: {
-			seconds: 1727076701,
-			nanoseconds: 672000000,
-		},
-		user_id: "iZRf4bthopb7vyuxqBmGIsCHbf12",
-		notes: "Hi",
-		id: "zERaUvaEeiw3Xk3J3dsf",
-	},
-];
 
 type PropsT<T extends Record<string, any>> = {
 	w: number;
@@ -80,7 +23,7 @@ type PropsT<T extends Record<string, any>> = {
 type Arc<T> = {
 	path: string;
 	data: T;
-	color: string;
+	color: string; //todo map category colour
 };
 
 export default function PieChart<T extends Record<string, any>>(
@@ -91,24 +34,6 @@ export default function PieChart<T extends Record<string, any>>(
 		margin = () => props.margin,
 		label = () => props.label,
 		data = () => props.data;
-
-	// const randomNumber = (Math.random() * 100).toFixed(0),
-	// 	divId = "my_dataviz" + randomNumber;
-
-	// onMount(() => {
-	// const svg = select(`div#${divId}`)
-	// 	.append("svg")
-	// 	// .attr("viewBox", "0 0 100 100")
-	// 	.attr("width", width)
-	// 	.attr("height", height)
-	// 	.append("g")
-	// 	.attr("transform", "translate(" + width() / 2 + "," + height() / 2 + ")")
-	// 	.append("circle")
-	// 	.attr("fill", getRandomColor())
-	// 	.attr("cx", "50")
-	// 	.attr("cy", "50")
-	// 	.attr("r", radius);
-	// });
 
 	const [arcs, setArcs] = createSignal<Arc<T>[]>([]);
 	const [hovered, setHovered] = createSignal<Arc<T> | null>(null);
